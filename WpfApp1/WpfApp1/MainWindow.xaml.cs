89bcb8eb38e0d3a94a42ef64cd4a970d420c679a
Jonas Steinmejer <jonas04@outlook.dk>
2023-08-30 09:26:15 +0200

    Update MainWindow.xaml.cs

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,200 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string connString = @"Data Source=ZBC-S-jona993p;Initial Catalog=Krølls_bank;User ID=PinManager;Password=admin;Integrated Security=True";
+
+        int i = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Numbers to pin
+        private void Number1_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 1;
+            }
+
+        }
+
+        private void Number2_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 2;
+            }
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 3;
+            }
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 4;
+            }
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 5;
+            }
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 6;
+            }
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 7;
+            }
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 8;
+            }
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 9;
+            }
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            if (PinCode.Text.Length < 4)
+            {
+                if (i == 0)
+                {
+                    PinCode.Text = "";
+                    i++;
+                }
+                PinCode.Text = PinCode.Text + 0;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            PinCode.Text = "";
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            int enteredPin;
+
+            if (int.TryParse(PinCode.Text, out enteredPin))
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+
+                        using (SqlCommand cmd = new SqlCommand("PinToClientInfo", conn))
+                        {
+                            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@Pin", enteredPin);
+
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+
+                                    // Show the existing Window1.xaml
+                                    Window1 window1 = new Window1();
+                                    window1.Show();
+                                    this.Close();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Invalid PIN. Please try again.");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid PIN format. Please enter numbers only.");
+            }
         }
     }
 }
